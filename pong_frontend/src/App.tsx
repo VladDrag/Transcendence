2ee69ad7b4d10b7677975bee12587aab531c742f
aionescu <aionescu@student.42wolfsburg.de>
2023-05-14 19:43:50 +0200

    Commit after rebase.

---
+++ pong_frontend/src/App.tsx
@@ -0,0 +1,42 @@
+// import React, {useState} from 'react';
+// import LogIn from './components/LogIn';
+// import UserStartPage from './components/UserStartPage';
+
+// const App = () => {
+//   const [userID, userID_set] = useState<string>(""); // number 0
+//   const [loged_in, loged_in_set] = useState<boolean>(false);
+
+ 
+//   if (!loged_in)
+//   {
+//     return (
+//       <LogIn userID_set={userID_set} loginDone_set={loged_in_set}  />
+//     );
+//   }
+//   else
+//   {
+//     return (
+//       <UserStartPage id={userID} />
+//     );
+//   }
+  
+// };
+
+// export default App;
+
+import React, { useState }  from 'react';
+import LogIn from './components/LogIn';
+import UserStartPage from './components/UserStartPage';
+
+function App() {
+  const [userID, userID_set] = useState<string>('');
+  const [loged_in, loged_in_set] = useState<boolean>(false);
+
+  if (!loged_in) {
+	return <LogIn userID_set={userID_set} loginDone_set={loged_in_set} />;
+  } else {
+    return <UserStartPage id={userID} />;
+  }
+}
+
+export default App;
